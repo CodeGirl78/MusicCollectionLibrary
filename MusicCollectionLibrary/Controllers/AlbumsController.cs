454f61f484a3d70b9674bae801174924bf1d8bc1
Melinda <codegirl78@gmail.com>
2021-12-07 16:55:54 -0600

    Code clean up

--- MusicCollectionLibrary/Controllers/AlbumsController.cs
+++ MusicCollectionLibrary/Controllers/AlbumsController.cs
@@ -8,15 +8,11 @@ namespace MusicCollectionLibrary.Controllers
 {
     public class AlbumsController : Controller
     {
-        static private List<string> Albums = new List<string>();
+        private static List<string> Albums = new List<string>();
         // GET: /<controller>/
+        [HttpGet]
         public IActionResult Index()
         {
-            List<string> Events = new List<string>();
-            Albums.Add("Jonny Lang");
-            Albums.Add("Jim Brickman");
-            Albums.Add("Dolly Parton");
-
             ViewBag.albums = Albums;
 
             return View();
