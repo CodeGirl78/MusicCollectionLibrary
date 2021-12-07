f56c6d8937401993e364eff87e057b51bba70af8
Melinda <codegirl78@gmail.com>
2021-12-07 17:58:51 -0600

    Added an Album model class with a name property including a constructor

---
+++ MusicCollectionLibrary/Models/Album.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicCollectionLibrary.Models
+{
+    public class Album
+    {
+        public string FirstName { get; set; }
+
+        public Album(string firstname)
+        {
+            FirstName = firstname;
+        }
+    }
+}
