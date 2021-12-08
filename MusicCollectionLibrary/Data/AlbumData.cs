a949198c1df406be5712cee9a93d515b7492435f
Melinda <codegirl78@gmail.com>
2021-12-07 19:32:14 -0600

    Added a new dictionary with method methods for the AlbumsData class

---
+++ MusicCollectionLibrary/Data/AlbumData.cs
@@ -0,0 +1,23 @@
+﻿using MusicCollectionLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicCollectionLibrary.Data
+{
+    public class AlbumData
+    {
+        // Store albums
+        static private Dictionary<int, Album> Albums = new Dictionary<int, Album>();
+
+        // Add albums
+        
+        // Retrieve all albums
+        
+        //Retrieve a single album
+       
+        // Remove an album from collection
+        
+    }
+}
