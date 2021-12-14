1926bba111b4cd8b0a6f3785acc58a33611ec9bb
Melinda <codegirl78@gmail.com>
2021-12-14 17:09:06 -0600

    Added a ViewModel with a class

---
+++ MusicCollectionLibrary/ViewModels/AddAlbumViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicCollectionLibrary.ViewModels
+{
+    public class AddAlbumViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string GroupName { get; set; }
+        public string AlbumTitle { get; set; }
+        public string Genre { get; set; }
+        public string AlbumFormat { get; set; }
+    }
+}
