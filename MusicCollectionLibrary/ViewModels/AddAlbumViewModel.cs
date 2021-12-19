e637061a658a845384115e2d1f5e0d889df1d546
Melinda <codegirl78@gmail.com>
2021-12-19 13:57:38 -0600

    Added span tag for error messages

--- MusicCollectionLibrary/ViewModels/AddAlbumViewModel.cs
+++ MusicCollectionLibrary/ViewModels/AddAlbumViewModel.cs
@@ -12,10 +12,10 @@ namespace MusicCollectionLibrary.ViewModels
         public string FirstName { get; set; }
         [StringLength(50, MinimumLength = 1)]
         public string LastName { get; set; }
-        [StringLength(100, MinimumLength = 3)]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Group Name must be between 3 and 100 characters")]
         public string GroupName { get; set; }
         [Required(ErrorMessage = "Album Title is required.")]
-        [StringLength(100, MinimumLength = 3, ErrorMessage = "Album Title must be between 3 and 150 characters.")]
+        [StringLength(150, MinimumLength = 3, ErrorMessage = "Album Title must be between 3 and 150 characters")]
         public string AlbumTitle { get; set; }
         public string Genre { get; set; }
         public string Format { get; set; }
