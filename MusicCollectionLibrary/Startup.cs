ec42994e05b93d5449c62e96e2f6b7f9c66af7ea
Melinda <codegirl78@gmail.com>
2022-01-04 15:13:46 -0600

    Added services to register as a data store

--- MusicCollectionLibrary/Startup.cs
+++ MusicCollectionLibrary/Startup.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MusicCollectionLibrary.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,8 @@ namespace MusicCollectionLibrary
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<AlbumDbContext>(options =>
+                   options.UseMySql(Configuration.GetConnectionString("DefaultConnection"), new MySqlServerVersion(new Version())));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
