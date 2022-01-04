79172ca2d88843137d346544ed07afc67729a524
Melinda <codegirl78@gmail.com>
2022-01-04 14:58:52 -0600

    Created a persistent data store for Album class

---
+++ MusicCollectionLibrary/Data/AlbumDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MusicCollectionLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MusicCollectionLibrary.Data
+{
+    public class AlbumDbContext : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+
+        public AlbumDbContext(DbContextOptions<AlbumDbContext> options)
+              : base(options)
+        {
+        }
+    }
+}
