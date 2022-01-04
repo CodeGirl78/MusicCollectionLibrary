53cecbf516208ca73c6d1979e63c92f7ea889350
Melinda <codegirl78@gmail.com>
2022-01-04 17:39:42 -0600

    Added Authorize attribute to AlbumsController

--- MusicCollectionLibrary/Controllers/AlbumsController.cs
+++ MusicCollectionLibrary/Controllers/AlbumsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using MusicCollectionLibrary.Data;
 using MusicCollectionLibrary.Models;
 using MusicCollectionLibrary.ViewModels;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace MusicCollectionLibrary.Controllers
 {
+    [Authorize]
     public class AlbumsController : Controller
     {
         private AlbumDbContext context;
